afdd9dcdabe022792b8c7fc95d75b7fad4e832f3
ASHA-XD <125123443+ASHA-XD@users.noreply.github.com>
2023-02-11 15:10:50 +0530

    Update help.js

--- commands/core/help.js
+++ commands/core/help.js
@@ -11,10 +11,10 @@ module.exports = {
         const embed = new EmbedBuilder()
         .setColor('#ff0000')
         .setAuthor({ name: client.user.username, iconURL: client.user.displayAvatarURL({ size: 1024, dynamic: true }) })
-        .setDescription('This code comes from a GitHub project [DOMINATOR-XD/Music-bot](https://github.com/DOMINATOR-XD/Music-bot).\nThe use of this one is possible while keeping the credits for free.\nIf you want to remove the credits join the Discord support server.')
+        .setDescription('This code comes from a GitHub project [ASHA-XD/Music-bot](https://github.com/ASHA-XD/Music-bot).\nThe use of this one is possible while keeping the credits for free.\nIf you want to remove the credits join the Discord support server.')
         .addFields([ { name: `Enabled - ${commands.size}`, value: commands.map(x => `\`${x.name}\``).join(' | ') } ])
         .setTimestamp()
-        .setFooter({ text: 'Music comes first - Made with heart by N1xDOMINATOR ❤️', iconURL: inter.member.avatarURL({ dynamic: true })});
+        .setFooter({ text: 'Music comes first - Made with heart by ASHA ❤️', iconURL: inter.member.avatarURL({ dynamic: true })});
 
         inter.reply({ embeds: [embed] });
     },
